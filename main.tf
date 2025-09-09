b1bf4d62d38362c2bd966ea2f26ed7e4078accef
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-09-09 11:25:22 -0400

    practice

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 module "vpc" {
     source = "../terraform-aws-vpc"
-    project = "roboshop"
-    environment = "dev"
-  
+    environment = var.environment
+    project = var.project
+    public_subnet_cidrs = var.public_subnent_cidrs
+    private_subnet_cidrs = var.private_subnent_cidrs
+    database_subnet_cidrs = var.database_subnent_cidrs
+    is_peering_required = true
 }
